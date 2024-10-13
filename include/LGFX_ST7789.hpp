5b51a4df0bb479f1aff3adc495903e2cc7274d81
stonedev-app <54230974+stonedev-app@users.noreply.github.com>
2024-10-13 22:10:29 +0900

    Added the definition for ST7789

---
+++ include/LGFX_ST7789.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#define LGFX_USE_V1
+#include <LovyanGFX.hpp>
+
+class LGFX_ST7789 : public lgfx::LGFX_Device
+{
+    lgfx::Panel_ST7789         _panel_instance;
+    lgfx::Bus_SPI              _bus_instance;
+
+  public:
+    LGFX_ST7789(void)
+    {
+      {
+        auto cfg = _bus_instance.config();
+        cfg.spi_host   = 1;
+        cfg.spi_mode   = 0;
+        cfg.freq_write = 10000000;
+        cfg.pin_sclk   = 10;
+        cfg.pin_miso   = -1;
+        cfg.pin_mosi   = 11;
+        cfg.pin_dc     = 8;
+        _bus_instance.config(cfg);
+        _panel_instance.setBus(&_bus_instance);
+      }
+
+      {
+        auto cfg = _panel_instance.config();
+        cfg.pin_cs           =    9;
+        cfg.pin_rst          =    12;
+        cfg.panel_width      =   240;
+        cfg.panel_height     =   240;
+        cfg.offset_x         =     0;
+        cfg.offset_y         =     0;
+        cfg.offset_rotation  =     0;
+        _panel_instance.config(cfg);
+      }
+
+      setPanel(&_panel_instance);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
+#if defined(SSD1306)
 #include "LGFX_SSD1306.hpp"
-// #include "LGFX_GC9A01.hpp"
-
 static LGFX_SSD1306 lcd;
-// static LGFX_GC9A01 lcd;
+#elif defined(GC9A01)
+#include "LGFX_GC9A01.hpp"
+static LGFX_GC9A01 lcd;
+#elif defined(ST7789)
+#include "LGFX_ST7789.hpp"
+static LGFX_ST7789 lcd;
+#endif
 
 void setup() {
   lcd.init();
